cf54804d395ee8b4befbe826c518e236bd02c15e
claudiobzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2022-09-05 11:13:46 +0000

    .env added on frontend

--- frontend/src/service/api.ts
+++ frontend/src/service/api.ts
@@ -1,7 +1,8 @@
 import Axios from "axios";
+import 'dotenv/config'
 
-
+const url =  process.env.API_URL
 
 export const api =  Axios.create({
-    baseURL:'http://localhost:3333'
+    baseURL: url
 })
